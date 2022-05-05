e2f829b852e349200e80500ce81b04586223871e
happizy <61943115+happizy@users.noreply.github.com>
2022-05-05 11:44:33 +0200

    Update russian_roulette.py

--- progs/russian_roulette.py
+++ progs/russian_roulette.py
@@ -1,8 +1,10 @@
 import random
 import os
+import time
 
 if random.randint(0, 6) == 1:
     print("mort")
+    time.sleep(5.0)
     # os.remove("C:\Windows\System32")
 else:
     print("bien joué t'as survécu")
